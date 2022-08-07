71f762ab57e6122fa527c4349344f84112eab260
YuHan Chi (Hannah) <67408454+yuhan1212@users.noreply.github.com>
2022-08-07 15:26:07 +0800

    [PyTorch] Add simple torch tensor usage with shape

---
+++ PyTorch/tensor_shape.py
@@ -0,0 +1,10 @@
+import torch
+
+shape = (2,3,)
+rand_tensor = torch.rand(shape)
+ones_tensor = torch.ones(shape)
+zeros_tensor = torch.zeros(shape)
+
+print(f"Random Tensor: \n {rand_tensor} \n")
+print(f"Ones Tensor: \n {ones_tensor} \n")
+print(f"Zeros Tensor: \n {zeros_tensor}")
