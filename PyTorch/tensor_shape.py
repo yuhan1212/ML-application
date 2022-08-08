dbf9ea34c2c8eb9d083830cdc34cf63e64d74ccd
YuHan Chi (Hannah) <67408454+yuhan1212@users.noreply.github.com>
2022-08-08 11:13:38 +0800

    [PyTorch] Add sample output for the shape tensor

--- PyTorch/tensor_shape.py
+++ PyTorch/tensor_shape.py
@@ -8,3 +8,19 @@ zeros_tensor = torch.zeros(shape)
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+"""
+
+Random Tensor:
+ tensor([[0.0034, 0.3447, 0.5966],
+        [0.9442, 0.1058, 0.2222]])
+
+Ones Tensor:
+ tensor([[1., 1., 1.],
+        [1., 1., 1.]])
+
+Zeros Tensor:
+ tensor([[0., 0., 0.],
+        [0., 0., 0.]])
+        
+"""
