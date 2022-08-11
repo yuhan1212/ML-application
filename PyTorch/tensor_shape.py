c826644c7702d5eb1ff708e14b23f257c1f8ad43
YuHan Chi (Hannah) <67408454+yuhan1212@users.noreply.github.com>
2022-08-12 06:47:59 +0800

    [PyTorch] Add attributes of a Tensor

--- PyTorch/tensor_shape.py
+++ PyTorch/tensor_shape.py
@@ -25,3 +25,9 @@ Output:
           [0., 0., 0.]])
         
 """
+
+tensor = torch.rand(3,4)
+
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
